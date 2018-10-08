9890256248f4e7c7e09b4c6e555c9061282a8e86
mallasudhakar <43954460+mallasudhakar@users.noreply.github.com>
2018-10-08 15:42:21 +0530

    Update fileerror.py

--- fileerror.py
+++ fileerror.py
@@ -1,8 +1,12 @@
+# Usage of finally in Exception handling in Python.
+#create a file with name hello in write mode
 try:
     f=open("hello.txt","w")
+    #here file is created and generated ZeroDivisionError
     x=1/0
 except FileNotFoundError:
     print("First create the file")
 finally:
+    #This block is always executed.
     f.close()
     
